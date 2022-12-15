0ccc4863e1a7079cc4c34b4eb5356fe0c0f87544
“Frank.ku” <frank.ku@fiadigi.com>
2022-12-15 21:37:16 +0800

    install docker shell script

--- shell/install_docker.sh
+++ shell/install_docker.sh
@@ -1,36 +1,23 @@
 #!/bin/bash
 
-# if os is not ubuntu or debian, exit
-if ! grep -q "Ubuntu" /etc/issue && ! grep -q "Debian" /etc/issue;
-  then
-    echo "This script only works on Ubuntu or Debian"
-    exit 1
-fi
+sudo apt-get install \
+     ca-certificates \
+     curl \
+     gnupg \
+     lsb-release -y
 
-# install docker
-sudo apt-get -y install ca-certificates curl wget gnupg lsb-release
 sudo mkdir -p /etc/apt/keyrings
-# check if ubuntu or debian
-if grep -q "Ubuntu" /etc/issue; 
-then
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg  
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-else
-  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg    
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null  
-fi
-sudo chmod a+r /etc/apt/keyrings/docker.gpg  
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-#check compose version and install compose
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
 VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d') &&
 DESTINATION=/usr/local/bin/docker-compose &&
+
 sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION &&
 sudo chmod 755 $DESTINATION
-
-sudo usermod -aG docker $USER
-sudo systemctl enable docker
